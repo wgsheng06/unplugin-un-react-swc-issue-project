7fe1519aada2df0ed363862833f0a751dc9dcaea
wgsheng_6 <511436962@qq.com>
2023-05-23 21:12:14 +0800

    feat: 新增unplugin-auto-import issue工程

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  useEffect(() => {
+    console.log("I Can't Be Browser Debug ")
+  })
+  return (
+    <>
+      <div>Show Code</div>
+    </>
+  )
+}
+
+export default App
